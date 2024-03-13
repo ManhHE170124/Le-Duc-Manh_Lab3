0ec161966ac8a28a5b471677f43e22a10d21153a
ManhHE170124 <manhldhe170124@fpt.edu.vn>
2024-03-13 16:26:28 +0700

    Add project files.

---
+++ AutomobileLibrary/DataAccess/MyStockContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomobileLibrary.DataAccess;
+
+public partial class MyStockContext : DbContext
+{
+    public MyStockContext()
+    {
+    }
+
+    public MyStockContext(DbContextOptions<MyStockContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Car> Cars { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { 
+        if(!optionsBuilder .IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-K7P5DH5\\SA; uid=SA; pwd=12345; database=MyStoke; TrustServerCertificate = true; Integrated Security = true");
+
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Car>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.CarId).HasColumnName("CarID");
+            entity.Property(e => e.CarName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Manufacturer)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("money");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ AutomobileLibrary/Repository/CarRepository.cs
@@ -0,0 +1,26 @@
+﻿using AutomobileLibrary.DataAccess;
+
+
+namespace AutomobileLibrary.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+      
+
+        public Car GetCarByID(int carId) => CarDAO.Instance.getCarByID(carId);
+        public IEnumerable<Car> GetCars() => CarDAO.Instance.GetCarList();
+        public void InSertCar(Car car) => CarDAO.Instance.AddNew(car);
+        public void DeleteCar(int carId) => CarDAO.Instance.Remove(carId);
+        public void UpdateCar(Car car) => CarDAO.Instance.Update(car);
+
+        public Car GetCarById(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
